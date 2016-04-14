819bc8157584110bb809d75ec22ed055af946983
Ryan Brackenbury <ryan.brackenbury@gmail.com>
2016-04-14 01:57:51 -0400

    Clean up generation to be less picky

--- plugins/intern/markov.py
+++ plugins/intern/markov.py
@@ -56,9 +56,18 @@ class Markov(object):
     def generate_markov_response(self, seed_word=None, next_word=None, size=25):
         w1, w2 = seed_word, next_word
         gen_words = []
-        for i in xrange(size):
-            gen_words.append(w1)
-            w1, w2 = w2, random.choice(self.cache[(w1, w2)])
-        gen_words.append(w2)
+        try:
+            for i in xrange(size):
+                gen_words.append(w1)
+                w1, w2 = w2, random.choice(self.cache[(w1, w2)])
+            gen_words.append(w2)
+        except:
+            seed = self.words.index(next_word)
+            seed_word = self.words[seed-1]
+            w1, w2 = seed_word, next_word
+            for i in xrange(size):
+                gen_words.append(w1)
+                w1, w2 = w2, random.choice(self.cache[(w1, w2)])
+            gen_words.append(w2)
         return ' '.join(gen_words)
 
